50ed793bce5653e4aa8026a1c3688cfe467aa43f
y0sif <yosifsoli981@gmail.com>
2026-05-07 01:31:16 +0300

    release v0.1.12
    Workspace extraction: five reusable crates published to crates.io,
    whisrs rewired to consume them. No user-visible behavior change.

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "whisrs";
-          version = "0.1.11";
+          version = "0.1.12";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
